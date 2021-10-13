bef67911f4fdbba668f60fa87e084c69df10e8ae
ymclapp <ymclapp@yahoo.com>
2021-10-13 10:56:02 -0500

    updated and almost back to where I lost info

--- Lab06Console/Beatles.cs
+++ Lab06Console/Beatles.cs
@@ -8,13 +8,18 @@ namespace Lab06Console
 {
   public class Beatles : Pop//child of Pop
   {
-    public Beatles() : base("Beatles", true)//call the base constructor
+    public Beatles() : base("The Beatles", true)//call the base constructor
     {
     }
     public override bool DeadMembers => true;
     public override string Songs()
     {
       return ("Hard Days Night, Yellow Submarine, Help");
+    }
+    public override string PrintMembers()
+    {
+      Console.WriteLine("The members of the band are: ");
+      return ("Paul McCartney, John Lennon, Ringo Starr, George Harrison");
     }
   }
 }
